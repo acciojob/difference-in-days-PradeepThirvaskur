7b14f5bc7b2768a2def1e8ddcc50bdc53555cee5
pradeep thirvaskur <142670238+PradeepThirvaskur@users.noreply.github.com>
2024-06-27 14:19:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-var dateDiffInDays = function (date1, date2) {
-  //   write your code here
-};
+// var dateDiffInDays = function (date1, date2) {
+//   //   write your code here
+// };
 
-// Do not change the code below.
-const dateOne = prompt("Enter Start Date.");
-const dateTwo = prompt("Enter End Date.");
-alert(dateDiffInDays(dateOne, dateTwo));
+// // Do not change the code below.
+// const dateOne = prompt("Enter Start Date.");
+// const dateTwo = prompt("Enter End Date.");
+// alert function dateDiffInDays(date1,date2){
+//         let firstDate= new Date(dateOne);
+//         let secondDate= new Date(dateTwo);
+
+//         const diffInTime=secondDate.getTime() - firstDate.getTime();
+        
+//         const diffIndays= diffInTime/(1000*3600*24);
+
+//         return diffIndays;
+//        }
+      
